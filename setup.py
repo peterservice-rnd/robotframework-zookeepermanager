d2e2fc30454485621bc6d58d6a23c93916d0b129
Aleskarova, Dinara <dinara.aleskarova@simbirsoft.com>
2018-10-17 15:04:54 +0400

    Version 1.0.2

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='robotframework-zookeepermanager',
-    version='1.0.1',
+    version='1.0.2',
     description='Robot Framework library for working with Apache Zookeeper.',
     long_description=long_description,
     url='https://github.com/peterservice-rnd/robotframework-zookeepermanager',
